f05c482f3584954bde65735ca60453841826d164
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-02-12 12:17:18 -0800

    Set up App.js by importing Game and Navbar. Export Game.js and navbar

--- memorygame/src/App.js
+++ memorygame/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Game from "./pages/Game";
+import Navbar from "./components/Navbar";
+// import  from "./components/";
+// import  from "./components/";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        {/* <Wrapper> */}
+          <Route exact path="/" component={Game} />
+        {/* </Wrapper>
+        <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ memorygame/src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">
+                Memory Game
+      </Link>
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item center">
+                        Click an image to begin!
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
