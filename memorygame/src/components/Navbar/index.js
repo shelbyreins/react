bdd47d723a4c1c921127f02f2d34e19106abe88c
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-02-18 08:58:48 -0800

    Take out unneccessary imports in navbar

--- memorygame/src/components/Navbar/index.js
+++ memorygame/src/components/Navbar/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./style.css";
-import { checkPropTypes } from "prop-types";
+
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar(props) {
