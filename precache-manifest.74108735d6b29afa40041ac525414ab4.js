327b1e81a0e7fa394a143b4f0f1046578094d6d2
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-02-19 22:07:46 -0800

    Updates

---
+++ precache-manifest.74108735d6b29afa40041ac525414ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b12bb879e327171273a8b80c90747c",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "888a3f905539f9a8d896",
+    "url": "/react/static/css/main.a528f654.chunk.css"
+  },
+  {
+    "revision": "540b9d313ea65e2f161d",
+    "url": "/react/static/js/2.986bd84b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react/static/js/2.986bd84b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888a3f905539f9a8d896",
+    "url": "/react/static/js/main.2cf1ab10.chunk.js"
+  },
+  {
+    "revision": "cd03f68d1ec864b06b93",
+    "url": "/react/static/js/runtime-main.f8026d47.js"
+  }
+]);
